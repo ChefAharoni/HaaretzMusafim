eff17b88d0628a49f5a31a3c8c648cb4ff1b3ccd
ChefAharoni <ChefAharoni@proton.me>
2024-01-04 18:51:22 -0600

    Added concat func to concat dates with url

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import datetime
-import datetime
+
+SITE_TXT = "site:"  # To access the site
+BASE_URL = (
+    "https://www.haaretz.co.il/magazine/"  # Starter of every musaf (magazine) url
+)
+
+add_ons = ["famous/", "ayelet-shani/"]
 
 # Get today's date
-today = datetime.date.today()
+TODAY = datetime.date.today()
 
 # Calculate the start date (20 years ago)
-start_date = today - datetime.timedelta(days=365 * 20)
+start_date = TODAY - datetime.timedelta(days=365 * 20)
 
 # Initialize an empty list to store the Friday dates
 fridays = []
 
-# Iterate over each date from the start date to today
-for date in (
-    start_date + datetime.timedelta(n) for n in range((today - start_date).days)
-):
-    # Check if the date is a Friday
-    if date.weekday() == 4:  # 4 represents Friday (Monday is 0 and Sunday is 6)
-        fridays.append(date)
+# Initialize an empty lize to store all the final links of the magazines (musafim)
+magazines_urls = []
+
+
+def add_fridays():
+    # Iterate over each date from the start date to today
+    for date in (
+        start_date + datetime.timedelta(n) for n in range((TODAY - start_date).days)
+    ):
+        # Check if the date is a Friday
+        if date.weekday() == 4:  # 4 represents Friday (Monday is 0 and Sunday is 6)
+            fridays.append(date)
 
 
 # Print the list of Friday dates
@@ -26,4 +37,18 @@ def print_fridays():
         print(friday)
 
 
-print(len(fridays))
+def concat_dates():
+    for friday in fridays:
+        mag_url = SITE_TXT + BASE_URL + str(friday) + "/"
+        magazines_urls.append(mag_url)
+    print(magazines_urls)
+
+
+def main():
+    add_fridays()
+    concat_dates()
+
+
+if __name__ == "__main__":
+    main()
+    print(len(fridays))  # Prints the length of the Fridays list
