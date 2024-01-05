1d3e3b222fa60981ded32f0a3550dd7b2532ba09
ChefAharoni <ChefAharoni@proton.me>
2024-01-05 07:10:38 -0600

    Changed from Fridays to Thursdays; need to add Wednesdays in addition

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ TODAY = datetime.date.today()
 # Calculate the start date (20 years ago)
 start_date = TODAY - datetime.timedelta(days=365 * 20)
 
-# Initialize an empty list to store the Friday dates
-fridays = []
+# Initialize an empty list to store the thursday dates
+thursdays = []
 
 # Initialize an empty lize to store all the final links of the magazines (musafim)
 magazines_urls = []
@@ -26,52 +26,52 @@ addon_urls = []  # Empty list for the addons
 magazines = {}  # Empty dictionary for the dirs that will be created
 
 
-def add_fridays():
+def add_thursdays():
     # Iterate over each date from the start date to today
     for date in (
         start_date + datetime.timedelta(n) for n in range((TODAY - start_date).days)
     ):
-        # Check if the date is a Friday
-        if date.weekday() == 4:  # 4 represents Friday (Monday is 0 and Sunday is 6)
-            fridays.append(date)
+        # Check if the date is a thursday
+        if date.weekday() == 3:  # 3 represents Thursday (Monday is 0 and Sunday is 6)
+            thursdays.append(date)
 
 
-def print_fridays():
-    # Print the list of Friday dates
-    print("Fridays:")
-    for friday in fridays:
-        print(friday)
+def print_thursdays():
+    # Print the list of thursday dates
+    print("thursdays:")
+    for thursday in thursdays:
+        print(thursday)
 
 
 def concat_dates():
-    for friday in fridays:
-        this_friday_articles = (
+    for thursday in thursdays:
+        this_thursday_articles = (
             []
-        )  # Saving as a list so values can be appended and added to each key of a Friday
-        mag_url = GOOGLE_SEARCH_URL + SITE_TXT + BASE_URL + str(friday) + "/"
-        this_friday_articles.append(mag_url)
+        )  # Saving as a list so values can be appended and added to each key of a thursday
+        mag_url = GOOGLE_SEARCH_URL + SITE_TXT + BASE_URL + str(thursday) + "/"
+        this_thursday_articles.append(mag_url)
         # magazines_urls.append(mag_url)
         for addon in add_ons:
             mag_url = (
-                GOOGLE_SEARCH_URL + SITE_TXT + BASE_URL + addon + str(friday) + "/"
+                GOOGLE_SEARCH_URL + SITE_TXT + BASE_URL + addon + str(thursday) + "/"
             )
-            this_friday_articles.append(mag_url)
+            this_thursday_articles.append(mag_url)
             # addon_urls.append(mag_url)
-        magazines[str(friday)] = this_friday_articles
+        magazines[str(thursday)] = this_thursday_articles
 
 
 def merge_urls():
-    # Saves the magazines dictionary into a JSON file called "Fridays.json".
-    with open("Fridays.json", "w") as file:
+    # Saves the magazines dictionary into a JSON file called "thursdays.json".
+    with open("Thursdays.json", "w") as file:
         json.dump(magazines, file, indent=4)
 
 
 def main():
-    add_fridays()
+    add_thursdays()
     concat_dates()
     merge_urls()
 
 
 if __name__ == "__main__":
     main()
-    print(len(fridays))  # Prints the length of the Fridays list
+    print(len(thursdays))  # Prints the length of the thursdays list
