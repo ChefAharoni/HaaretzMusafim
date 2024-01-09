daa255604d45640d72bf514c2b89f2d3684293e8
Aharoni <ChefAharoni@proton.me>
2024-01-09 15:33:33 +0000

    Updated script that auto-commits and pushes the titled_urls.json every 5 minutes

--- scripts/git_update_json.sh
+++ scripts/git_update_json.sh
@@ -3,14 +3,21 @@
 # Repeat the process indefinitely
 while true; do
     # Add all new or modified files to the staging area
-    git add .
+    git add data/titled_urls.json
 
     # Commit the changes with a message
-    git commit -m "update"
+    git commit -m "Auto-Updated titled.urls.json"
 
     # Push the changes to the remote repository
     git push
 
-    # Wait for 5 minutes (300 seconds)
-    sleep 300
+    # Set the countdown time (5 minutes = 300 seconds)
+    countdown=300
+
+    # Countdown loop
+    while [ $countdown -gt 0 ]; do
+        echo "Time remaining before next run: $countdown seconds"
+        sleep 10
+        ((countdown-=10))
+    done
 done
